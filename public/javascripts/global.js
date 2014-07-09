c7cb210dd58c86a26b451392b577c600932279ec
Bibodha Neupane <bibodha@gmail.com>
2014-07-09 14:41:18 -0600

    Added school to the form and it shows up on the table

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,185 +1,182 @@
 var SigninModel = function () {
-	var self = this,
-		user = {},
-		userPropertyList = [
-			'firstname',
-			'lastname',
-			'street',
-			'city',
-			'state',
-			'zip',
-			'dateOfBirth',
-			'gender',
-			'school'
-		],
-		grid,
-		columns = [
-			{id: "firstname", name: "First Name", field: "firstname", minWidth: 50},
-			{id: "lastname", name: "Last Name", field: "lastname"},
-			{id: "street", name: "Street", field: "street"},
-			{id: "city", name: "City", field: "city"},
-			{id: "state", name: "State", field: "state"},
-			{id: "zip", name: "Zip", field: "zip"},
-			{id: "dateOfBirth", name: "Date Of Birth", field: "dateOfBirth"},
-			{id: "gender", name: "Gender", field: "gender"},
-			{id: "school", name: "School", field: "school"}
-		],
-		options = {
-			enableCellNavigation: true,
-			enableColumnReorder: false,
-		};
+    var self = this,
+        user = {},
+        userPropertyList = [
+            'firstname',
+            'lastname',
+            'street',
+            'city',
+            'state',
+            'zip',
+            'dateOfBirth',
+            'gender',
+            'school'
+        ],
+        grid,
+        columns = [
+            {id: "firstname", name: "First Name", field: "firstname", minWidth: 50},
+            {id: "lastname", name: "Last Name", field: "lastname"},
+            {id: "street", name: "Street", field: "street"},
+            {id: "city", name: "City", field: "city"},
+            {id: "state", name: "State", field: "state"},
+            {id: "zip", name: "Zip", field: "zip"},
+            {id: "dateOfBirth", name: "Date Of Birth", field: "dateOfBirth"},
+            {id: "gender", name: "Gender", field: "gender"},
+            {id: "school", name: "School", field: "school"}
+        ],
+        options = {
+            enableCellNavigation: true,
+            enableColumnReorder: false,
+        };
 
-	self.getUserObjectFromForm = function(){
-		var output = {};
-		userPropertyList.forEach(function(fieldName){
-			output[fieldName] = $('#input-' + fieldName).val();
-		});
-		return output;
-	};
-	self.setUserObjectToForm = function(user){
-		userPropertyList.forEach(function(fieldName){
-			$('#input-' + fieldName).val(user[fieldName]);
-		});
-	};
+    self.getUserObjectFromForm = function(){
+        var output = {};
+        userPropertyList.forEach(function(fieldName){
+            output[fieldName] = $('#input-' + fieldName).val();
+        });
+        return output;
+    };
+    self.setUserObjectToForm = function(user){
+        userPropertyList.forEach(function(fieldName){
+            $('#input-' + fieldName).val(user[fieldName]);
+        });
+    };
 
-	self.getUserData = function() {
-		var items = '';
-		$.getJSON(' /users/userList', function(data){
-			items = data;
-		}).done(function(data){
-			console.log(data);
-		});
-	};
+    self.getUserData = function() {
+        var items = '';
+        $.getJSON(' /users/userList', function(data){
+            items = data;
+        }).done(function(data){
+            console.log(data);
+        });
+    };
 
-	self.populateTable = function () {
-		var tableContent = '';
-		$.getJSON( '/users/userlist', function( data ) {
-			userListData = data;
+    self.populateTable = function () {
+        var tableContent = '';
+        $.getJSON( '/users/userlist', function( data ) {
+            userListData = data;
 
-			grid = new Slick.Grid('#myGrid', data, columns, options);
-		});
-	};
+            grid = new Slick.Grid('#myGrid', data, columns, options);
+        });
+    };
 
-	self.showUserInfo = function (event) {
-		event.preventDefault();
-		var thisUserName = $(this).attr('rel'),
-		arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName),
-		thisUserObject = userListData[arrayPosition];
+    self.showUserInfo = function (event) {
+        event.preventDefault();
+        var thisUserName = $(this).attr('rel'),
+        arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName),
+        thisUserObject = userListData[arrayPosition];
 
-		$('#userInfoName').text(thisUserObject.fullname);
-		$('#userInfoAge').text(thisUserObject.age);
-		$('#userInfoGender').text(thisUserObject.gender);
-		$('#userInfoLocation').text(thisUserObject.location);
-	};
+        $('#userInfoName').text(thisUserObject.fullname);
+        $('#userInfoAge').text(thisUserObject.age);
+        $('#userInfoGender').text(thisUserObject.gender);
+        $('#userInfoLocation').text(thisUserObject.location);
+    };
 
-	self.addUser = function (event) {
-		event.preventDefault();
+    self.addUser = function (event) {
+        event.preventDefault();
 
-		var errorCount = 0;
-		$('#addUser input').each(function(index) {
-			if($(this).val() === '') { errorCount++; }
-		});
+        var errorCount = 0;
+        $('#addUser input').each(function(index) {
+            if($(this).val() === '') { errorCount++; }
+        });
 
-		if(errorCount === 0) {
+        if(errorCount === 0) {
 
-			var newUser = self.getUserObjectFromForm();
+            var newUser = self.getUserObjectFromForm();
 
-			console.log('New User:', newUser);
+            console.log('New User:', newUser);
 
-			$.ajax({
-				type: 'POST',
-				data: newUser,
-				url: '/users/adduser',
-				dataType: 'JSON'
-			}).done(function( response ) {
-				if (response.msg === '') {
+            $.ajax({
+                type: 'POST',
+                data: newUser,
+                url: '/users/adduser',
+                dataType: 'JSON'
+            }).done(function( response ) {
+                if (response.msg !== '') {
+                    alert('Error: ' + response.msg);
+                }
+            });
+        }
+        else {
+            alert('Please fill in all fields');
+            return false;
+        }
+    };
 
-				}
-				else {
-					alert('Error: ' + response.msg);
-				}
-			});
-		}
-		else {
-			alert('Please fill in all fields');
-			return false;
-		}
-	};
+    self.deleteUser = function (event) {
+        event.preventDefault();
+        var confirmation = confirm('Are you sure you want to delete this user?');
 
-	self.deleteUser = function (event) {
-		event.preventDefault();
-		var confirmation = confirm('Are you sure you want to delete this user?');
+        if (confirmation === true) {
+            $.ajax({
+                type: 'DELETE',
+                url: '/users/deleteuser/' + $(this).attr('rel')
+            }).done(function( response ) {
+                if (response.msg === '') {
+                }
+                else {
+                    alert('Error: ' + response.msg);
+                }
+                self.populateTable();
+            });
+        }
+        else {
+            return false;
+        }
+    };
 
-		if (confirmation === true) {
-			$.ajax({
-				type: 'DELETE',
-				url: '/users/deleteuser/' + $(this).attr('rel')
-			}).done(function( response ) {
-				if (response.msg === '') {
-				}
-				else {
-					alert('Error: ' + response.msg);
-				}
-				self.populateTable();
-			});
-		}
-		else {
-			return false;
-		}
-	};
+    self.editUser = function(event){
+        event.preventDefault();
+        edit = true;
+        $.ajax({
+            type: 'POST',
+            url: '/users/getuser/' + $(this).attr('rel')
+        }).done(function(res){
+            if(res.err === null){
+                var user = res.msg;
+                self.setUserObjectToForm(user);
+            }
+            else{
+                alert('Error: ' + res.msg);
+            }
+        });
+    };
 
-	self.editUser = function(event){
-		event.preventDefault();
-		edit = true;
-		$.ajax({
-			type: 'POST',
-			url: '/users/getuser/' + $(this).attr('rel')
-		}).done(function(res){
-			if(res.err === null){
-				var user = res.msg;
-				self.setUserObjectToForm(user);
-			}
-			else{
-				alert('Error: ' + res.msg);
-			}
-		});
-	};
-
-	self.signin = function(name){
-		$.ajax({
-			type: 'POST',
-			url: '/users/signin/' + name
-		}).done(function(res){
-			if(res.err){
-				alert(res.err);
-			}
-			else{
-				$('#signinTextBox').val('');
-			}
-		});
-	};
+    self.signin = function(name){
+        $.ajax({
+            type: 'POST',
+            url: '/users/signin/' + name
+        }).done(function(res){
+            if(res.err){
+                alert(res.err);
+            }
+            else{
+                $('#signinTextBox').val('');
+            }
+        });
+    };
 };
 
 var $body = $(document.body),
-	mySignin = new SigninModel();
+    mySignin = new SigninModel();
 
 mySignin.populateTable();
 
 $body.on('click', '#signinBtn', function(){
-	mySignin.signin($('#signinTextBox').val());
+    mySignin.signin($('#signinTextBox').val());
 });
 $body.on('click', '#formAddUserBtn', mySignin.addUser);
 $('#contentTab').tab();
 $body.on('click', '#contentTab a', function(e){
-	e.preventDefault();
-	$('#contentTab li').removeClass('active');
-	$('.tab-content div.tab-pane').removeClass('active');
-	$(this).addClass('active');
+    e.preventDefault();
+    $('#contentTab li').removeClass('active');
+    $('.tab-content div.tab-pane').removeClass('active');
+    $(this).addClass('active');
 });
 
 $('#input-dateOfBirth').mask('**/**/****');
 
 $('#input-dateOfBirth').datepicker({
-	autoclose: true
+    autoclose: true
 });
 
--- routes/users.js
+++ routes/users.js
@@ -2,58 +2,58 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/userlist', function(req, res){
-	var db = req.db;
-	db.collection('userlist').find().toArray(function(err, items){
-		res.json(items);
-	});
+    var db = req.db;
+    db.collection('userlist').find().toArray(function(err, items){
+        res.json(items);
+    });
 });
 
 router.post('/adduser', function(req, res){
-	var db = req.db;
-	db.collection('userlist').insert(req.body, function(err, result){
-		res.send(
-			(err === null) ? {msg: ''} : {msg: err}
-		);
-	});
+    var db = req.db;
+    db.collection('userlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? {msg: ''} : {msg: err}
+        );
+    });
 });
 
 router.delete('/deleteuser/:id', function(req, res){
-	var db = req.db;
-	var userToDelete = req.params.id;
-	db.collection('userlist').removeById(userToDelete, function(err, result){
-		res.send((result === 1) ? {msg: ''} : {msg: 'error: ' + err});
-	});
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('userlist').removeById(userToDelete, function(err, result){
+        res.send((result === 1) ? {msg: ''} : {msg: 'error: ' + err});
+    });
 });
 
 router.post('/getuser/:id', function(req, res){
-	var db = req.db,
-		userToFetch = req.params.id,
-		user = '';
+    var db = req.db,
+        userToFetch = req.params.id,
+        user = '';
 
-	db.collection('userlist').findById(userToFetch, function(err, result){
-		res.send((err === null) ? {err : null, msg: result} : {err : 1, msg: 'error: ' + err});
-	});
+    db.collection('userlist').findById(userToFetch, function(err, result){
+        res.send((err === null) ? {err : null, msg: result} : {err : 1, msg: 'error: ' + err});
+    });
 });
 
 router.post('/signin/:name', function(req, res){
-	var db = req.db,
-	date = new Date,
-	name = req.params.name,
-	user;
+    var db = req.db,
+    date = new Date,
+    name = req.params.name,
+    user;
 
-	db.collection('userlist').findOne({username: name}, function(err, result){
-		user = result;
-		db.collection('signin').insert({id: user._id, signinTime: date.toLocaleString()}, function(err, result){
-			if(err){
-				res.send('There was an error: ' + err);
-			}
-			else{
-				res.location("");
-				res.redirect("/");
-				res.send({msg: ''});
-			}
-		});
-	});
+    db.collection('userlist').findOne({username: name}, function(err, result){
+        user = result;
+        db.collection('signin').insert({id: user._id, signinTime: date.toLocaleString()}, function(err, result){
+            if(err){
+                res.send('There was an error: ' + err);
+            }
+            else{
+                res.location("");
+                res.redirect("/");
+                res.send({msg: ''});
+            }
+        });
+    });
 });
 
 module.exports = router;
