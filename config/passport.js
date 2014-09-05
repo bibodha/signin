5b14c3e6438f67c130791c0fee087a86d68056c3
Bibodha Neupane <bibodha@gmail.com>
2014-09-05 15:58:52 -0600

    Almost working, i think

--- config/passport.js
+++ config/passport.js
@@ -3,13 +3,9 @@
 // load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
 
-var mongo = require('mongoskin');
-var User = {};
-
 // expose this function to our app using module.exports
 module.exports = function(db, passport) {
 
-    User = db.collection('admin');
     // =========================================================================
     // passport session setup ==================================================
     // =========================================================================
@@ -23,39 +19,32 @@ module.exports = function(db, passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
+        db.collection('admin').findById(id, function(err, user) {
             done(err, user);
         });
     });
 
     //Local-Login
     passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'username',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, 
     function(req, username, password, done) { // callback with email and password from our form
-
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'username' :  username }, function(err, user) {
+        db.collection('admin').findOne({ 'username' :  username }, function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
-
             // if no user is found, return the message
             if (!user)
                 return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
             // if the user is found but the password is wrong
             if (!user.validPassword(password))
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
             // all is well, return successful user
             return done(null, user);
         });
-
     }));
-
 };
--- routes/login.js
+++ routes/login.js
@@ -1,41 +1,37 @@
 // app/login.js
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	router.get('/login', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('login');
-	});
+module.exports = function(app, passport){
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('login', {message: req.flash('loginMessage')});
+    });
 
-	// process the login form
-	router.post('/login', function(){
-      passport.authenticate('local-login', {
-  		successRedirect : '/', // redirect to the secure profile section
-  		failureRedirect : '/' // redirect back to the signup page if there is an error
-  	})
-  });
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/',
+            failureRedirect : '/login',// redirect back to the login page if there is an error
+            failureFlash : true
+        }));
 
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	router.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
 
-// route middleware to make sure
-function isLoggedIn(req, res, next) {
+    // route middleware to make sure
+    function isLoggedIn(req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated())
+            return next();
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-};
-
-module.exports = router;
+        // if they aren't redirect them to the home page
+        res.redirect('/login');
+    };
+}
